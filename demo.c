73268dd4267bc584efba3b5745b885314eec2bb2
Shahbaj <shahbajs987@gmail.com>
2022-06-02 22:57:19 -0400

    Add files via upload

--- demo.c
+++ demo.c
@@ -1,8 +1,52 @@
-/* Hello World
+#include <stdio.h>
+
+/* Variables and Types
+
+// C has several types of variales, but there are a few basic types:
+
+// Signed Integers- defined using char, int, short long, or long long
+
+// Unsigned Integers - defined using unsigned char, unsigned int, unsigned short,
+// unsigned long, or unsigned long long
+
+// Floating Point - defined using float and doubles
+
+// Structures - defined using struct
+
+// Note that C does not have a boolean type, it is usually defined using
+
+#define BOOL char
+#define FALSE 0
+#define TRUE 1
+
+// C strings consist of character arrays
+
+// Variables are defined first by type then name and if necessary value
+
+int main(){
+    int a = 2;
+    float b = 3.22;
+    double c = 12.9;
+    float product;
+
+    product = a * b * c;
+    printf("The product of a, b, and c is %f.\n", product);
+
+    // We can also use arithmetic operators
+
+    product = (product - a) * 2;
+    printf("Twice the product of b and c is %f.\n", product);
+
+
+    return 0;
+}
 
 */
 
-#include <stdio.h>
+
+
+
+/* Hello World
 
 // Every C program uses libraries, allowing us to execute necessary functions
 // For example, a basic function 'printf' prints text to the screen, and is defined 
@@ -18,3 +62,5 @@ int main(){
     printf("Hello, World!");
     return 0;
 }
+
+*/
