e042f1361aa10a1a8adc189e57222993e58e9bfa
Shahbaj <shahbajs987@gmail.com>
2022-06-03 19:18:56 -0400

    Add files via upload

--- demo.c
+++ demo.c
@@ -1,7 +1,118 @@
 #include <stdio.h>
 #include <math.h>
 
-/* Arrays
+/* MULTIDIMENSIONAL ARRAYS
+
+int main(){
+
+    // In the previous code, we looked at a one-dimensional array
+    // In C, we can also create multidimensional arrays, for example
+
+    int mtrx[1][2][3];
+
+    // We can also create it and define it as so
+
+    char a[1][5] = {
+        {'a', 'e', 'i', 'o', 'u'}
+    };
+
+
+    // TWO-DIMENSIONAL ARRAYS
+
+    // The simplest form of multidimensional array is the two-dimensional array
+    // A two-dimensional array is functionally a list of one-dimensional arrays
+
+    // Declaring a two-dimensional array takes the following form:
+    // type arrayName [x][y];
+
+    // type can be any C data type (int, char, long, long long, double, etc.)
+    // arrayName is an identifier
+    // Number of rows is given by [x]
+    // Number of columns is given by [y]
+
+    // Thus each element in the matrix is identified by the form:
+    // a[i][j]
+    // Where 'a' is the name of the array, 'i' specifies the row, and 'j' specifies the column
+
+    // In certain cases, the compiler can infer the value of [x] when provided a definition
+    // For example
+
+    char b[][5] = {
+        {'A', 'E', 'I', 'O', 'U'},
+        {'a', 'e', 'i', 'o', 'u'}
+    };
+
+    // However, the [y] value is still necessary, as the compiler cannot infer
+    // the number of values per list
+
+
+    // Let us practice initializing an array with four rows and five columns
+    int x[4][5] = {
+        {0, 1, 2, 3, 4}, // Initializers for row indexed by 0 
+        {5, 6, 7, 8, 9}, // Initializers for row indexed by 1 
+        {10, 11, 12, 13, 14}, // Initializers for row indexed by 2 
+        {15, 16, 17, 18, 19} // Initializers for row indexed by 3 
+    };
+
+    // The inside braces indicate the desired row and are optional
+    // The following initialization is functionally the same
+
+    int y[4][5] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+
+    // To access an element, we simply use the row and column indices
+
+    int val = y[3][4];
+
+    printf("The fifth element of the fourth row is %d", val);
+
+
+    return 0;
+}
+
+
+
+// The following code finds the average grades for a group of four students for two courses– 
+// Calculus and Physics
+
+// To do this we will use a two-dimensional array called grades
+// The grades corresponding to Calculus will be stored in the first row (grades[0])
+// The grades corresponding to Physics will be stored in the second row (grades[1])
+
+int main(){
+    short average;
+
+    int grades[2][4] = {
+        {95, 90, 85, 70},
+        {85, 75, 90, 95}
+    };
+
+    for(int i = 0; i < 2; i++){
+        average = 0;
+
+        for(int j = 0; j < 5; j++){
+            average += grades[i][j];
+        }
+
+        average /= 5.0;
+        printf("The average grade obtained in subject %d is: %.2hu\n", i, average);
+
+    }
+
+
+    return 0;
+}
+
+*/
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------
+
+
+
+
+/* ONE-DIMENSIONAL ARRAYS
 
 // Arrays can hold multiple values under the same variable name organized by index
 // Arrays in C are zero-based, meaning they begin at index 0
@@ -25,7 +136,7 @@ for(int i = 0; i < 7; i++){
 
 // This allows array access to be very efficient
 
-*/
+
 
 // The following code calls a method for printing the average of an array
 
@@ -53,13 +164,17 @@ int main(){
     return 0;
 }
 
+*/
+
 
 
 
 // --------------------------------------------------------------------------------------------------------------
 
 
-/* Variables and Types
+
+
+/* VARIABLES & TYPES
 
 // C has several types of variales, but there are a few basic types:
 
@@ -103,10 +218,14 @@ int main(){
 */
 
 
+
+
 // --------------------------------------------------------------------------------------------------------------
 
 
-/* Hello World
+
+
+/* HELLO WORLD
 
 // Every C program uses libraries, allowing us to execute necessary functions
 // For example, a basic function 'printf' prints text to the screen, and is defined 
@@ -124,3 +243,8 @@ int main(){
 }
 
 */
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------
